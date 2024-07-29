47e062f7dcb0a8eeacd655346657422ba5528aba
Pat Forde <patfordegn@gmail.com>
2024-07-29 12:40:59 +0000

    Add styling for questions and answers buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -81,7 +81,7 @@ function nameEntered () {
 let playerName = document.getElementById("playerName").value;
 console.log (playerName);
 displayQuiz ();
-document.getElementById("currentScore").innerHTML = "Welcome "  + playerName + " Let's get started!"
+document.getElementById("welcomePlayer").innerHTML = "Welcome "  + playerName + " Here is your first question"
 
 }
 
